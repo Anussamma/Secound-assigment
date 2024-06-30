700325333b4f50326c4780ea881bd331a3f743f4
Anussamma <anussamma586@gmail.com>
2024-06-30 15:39:07 +0500

    Add files via upload

---
+++ secound assigment.py
@@ -0,0 +1,37 @@
+
+#first assigment
+
+b = 4 
+h = 8
+
+print("area of triangle")
+print(0.5*b*h)
+
+#secound
+
+radius = 8
+pie = 3.14
+
+print("area of circle")
+print(pie*radius*radius)
+
+#third assigment
+s = 6
+
+print("area of square")
+print(s*s)
+                                                                                                                                                                                                                                           
+
+name=int(input("your name"))
+print(name)
+
+
+name=int(input("your number"))
+print(name)
+
+print("enter your value for multiply")
+x1 = int(input())
+
+print("secound value")
+x2 = int(input())
+print('you ans is ' + str(x1 * x2))
